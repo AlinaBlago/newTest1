700cfae341d8373180abe9af91d411a8d500e692
AlinaBlago <alinablago19@gmail.com>
2021-02-16 12:30:25 +0300

    git push

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -2,5 +2,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("first message");
         System.out.println("Without add");
+        System.out.println("Only git push");
     }
 }
