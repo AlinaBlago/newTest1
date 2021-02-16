60a7384858ecd07ae2ea3871a5a5173de363f8eb
AlinaBlago <alinablago19@gmail.com>
2021-02-16 12:41:45 +0300

    second developer' commit

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -3,5 +3,6 @@ public class TestMain {
         System.out.println("first message");
         System.out.println("Without add");
         System.out.println("Only git push");
+        System.out.println("Second developer has done first commit");
     }
 }
